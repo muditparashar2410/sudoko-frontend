290afaf7ed6f1cdbbecb51693db3eaff47a9ed4f
Mudit <muditparashar2410@gmail.com>
2023-07-04 12:36:22 +0530

    Update files

---
+++ src/Sudoko_Grid.js
@@ -0,0 +1,159 @@
+import React, { useState } from 'react';
+import './Sudoko_Grid.css';
+// import './SudoControl.js';
+import axios from 'axios';
+
+const Sudoku_Grid = () => {
+  const [board, setBoard] = useState([]);
+  const [selectedNumber, setSelectedNumber] = useState();
+  const [selectedCell, setSelectedCell] = useState(null);
+
+  // const generateBoard = () => {
+  //   const newBoard = Array(9).fill().map(() => Array(9).fill(''));
+  //   setBoard(newBoard);
+  // };
+  const handleStartClick = () => {
+    axios
+    .post('https://sudokobackend.azurewebsites.net/api/sudoku/start', 'START', {
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    })
+    .then(response => {
+      // console.log('Sudoku puzzle started:', response.data);
+        // Handle the response or update the state as needed
+
+        // After the first request is successful, make the second GET request
+        axios
+          .get('https://sudokobackend.azurewebsites.net/api/sudoku/generate')
+          .then(response => {
+            // console.log(response.data);
+            // const newBoard = Array(9).fill().map(() => Array(9).fill(''));
+            // Update the board state with the generated puzzle
+            // const responseData = JSON.parse(response.data);
+            //             const transformedData = response.data.puzzle.map((row) => row.map((cell) => cell.value));
+            // setBoard(transformedData);
+            // setBoard(newBoard);
+
+            setBoard(response.data.map(row => row.map(cell => cell === 0 ? '' : cell)));;
+          })
+          .catch(error => {
+            console.error('Failed to generate Sudoku puzzle:', error);
+            // Handle the error as needed
+          });
+      })
+      .catch(error => {
+        console.error('Failed to start Sudoku puzzle:', error);
+        // Handle the error as needed
+      });
+    };
+    const handleCellValueChange = (rowIndex, colIndex, value) => {
+      if (selectedNumber === '') {
+        setSelectedCell(null);
+        return;
+      }
+    
+      if (selectedCell !== null && selectedCell.row === rowIndex && selectedCell.col === colIndex) {
+        return;
+      }
+    
+      const newBoard = [...board];
+      newBoard[rowIndex][colIndex] = selectedNumber;
+      setBoard(newBoard);
+      setSelectedCell({ row: rowIndex, col: colIndex });
+    
+      const requestData = {
+        row: rowIndex,
+        column: colIndex,
+        value: selectedNumber
+      };
+    
+      axios.post('https://sudokobackend.azurewebsites.net/api/sudoku/fill', requestData, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => {
+          console.log('Cell filled:', response.data);
+          // Handle the response if needed
+        })
+        .catch(error => {
+          console.error('Failed to fill cell:', error);
+  // Reset the cell value to blank
+  const newBoard = [...board];
+  newBoard[rowIndex][colIndex] = '';
+  setBoard(newBoard);
+  setSelectedCell(null);
+  // Alert the user about the error
+  alert('Failed to fill the cell. Please try again.');
+
+        });
+    };
+    
+  const handleNumberSelect = (number) => {
+    setSelectedNumber(number);
+  };
+  const handleHintClick = () => {
+    axios.get('https://sudokobackend.azurewebsites.net/api/sudoku/hint')
+      .then(response => {
+        const hint = response.data;
+        if (hint) {
+          const { row, column, value } = hint;
+          alert(`Hint generated: Fill cell at row ${row}, column ${column} with ${value}`);
+        } else {
+          alert('No hint available.');
+        }
+      })
+      .catch(error => {
+        console.error('Failed to generate hint:', error);
+        // Handle the error as needed
+      });
+  };
+  return (
+    <div className="sudoku-container">
+    <div className="board-container">
+      <table className="board">
+        <tbody>
+          {board.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, colIndex) => (
+                <td
+                  key={colIndex}
+                  className={selectedCell && selectedCell.row === rowIndex && selectedCell.col === colIndex ? 'selected-cell' : ''}
+                  onClick={() => handleCellValueChange(rowIndex, colIndex)}
+                >
+                  {cell}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    <div className="number-selector">
+      <h3>Number Selector</h3>
+      <div className="number-buttons">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((number) => (
+          <button
+            key={number}
+            className={`number-button ${selectedNumber === number ? 'selected' : ''}`}
+            onClick={() => handleNumberSelect(number)}
+          >
+            {number}
+          </button>
+        ))}
+      </div>
+    </div>
+    <button className="generate-button" onClick={handleStartClick}>
+      Start/ResetGame
+    </button>
+    <button className="hint-button" 
+    onClick={handleHintClick}
+    >
+  Hint
+</button>
+  </div>
+  );
+};
+
+export default Sudoku_Grid;
